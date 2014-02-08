8f809ad46e8c1e61d0d6e76308865e4388a9bcdf
pmlynarczyk <zwarios@gmail.com>
2014-02-08 17:43:40 +0100

    Fix

--- src/main/scala/zwrss/pl/multialignment/Testowo.scala
+++ src/main/scala/zwrss/pl/multialignment/Testowo.scala
@@ -6,13 +6,11 @@ import zwrss.pl.multialignment.model._
 object Testowo {
   def main(args: Array[String]) {
 
-//    val s1 = Sequence(Seq(Ala, Arg, Glu, Gln, Gly, Ser, Thr, Trp, Tyr))
-//    val s2 = Sequence(Seq(Tyr, Ser, Gly, Ser, Thr, Trp, Gln))
-//    val s3 = Sequence(Seq(Ala, Arg, Thr, Trp, Tyr))
-    val s1 = Sequence(Seq(Ala, Empty, Arg, Glu))
-    val s2 = Sequence(Seq(Empty, Ala, Arg, Glu))
+    val s1 = Sequence(Seq(Ala, Arg, Glu, Gln, Gly, Ser, Thr, Trp, Tyr))
+    val s2 = Sequence(Seq(Tyr, Ser, Gly, Ser, Thr, Trp, Gln))
+    val s3 = Sequence(Seq(Ala, Arg, Thr, Trp, Tyr))
 
-    val swarm = new BeeSwarm(Seq(s1, s2), 200, 30, 100)
+    val swarm = new BeeSwarm(Seq(s1, s2, s3), 200, 30, 100)
 
     println(swarm.solution.score)
     println(swarm.solution.string)
